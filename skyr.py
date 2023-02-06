1a19b527446bf033d836b2c83d196d81c30faea9
Nikita Karamov <me@kytta.dev>
2023-02-06 18:33:32 +0100

    Add a very basic executable

---
+++ skyr.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+from typing import Sequence
+
+
+def main(argv: Sequence[str] | None = None) -> int:
+    return 0
+
+
+if __name__ == "__main__":
+    raise (SystemExit(main()))
